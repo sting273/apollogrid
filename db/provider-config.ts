916054c6ccbaa18689511474cd7dabb3a8448f24
Yiding Guo <guoyiding283@gmail.com>
2026-09-19 23:44:07 +0100

    Remove completed legacy data migration route

--- db/provider-config.ts
+++ db/provider-config.ts
@@ -1,6 +1,6 @@
 import { env } from "cloudflare:workers";
 
-type ProviderSecret = "IDEAL_POSTCODES_API_KEY" | "GOOGLE_MAPS_API_KEY" | "ADMIN_EMAILS" | "MIGRATION_TOKEN";
+type ProviderSecret = "IDEAL_POSTCODES_API_KEY" | "GOOGLE_MAPS_API_KEY" | "ADMIN_EMAILS";
 
 // Read worker bindings directly as well as Node compatibility environment.
 // Values remain on the server and must never be returned to the browser.
